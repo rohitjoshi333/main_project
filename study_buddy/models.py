045930a344df50a1ac77cfc20cdf5d4c36a3852c
rohitjoshi333 <158034481+rohitjoshi333@users.noreply.github.com>
2025-08-15 14:03:53 +0545

    fixed bugs

--- study_buddy/models.py
+++ study_buddy/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Interest(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True, default="no interest")  # unique optional
 
     def __str__(self):
         return self.name
